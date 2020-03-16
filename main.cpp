df065560a8f22217ad342cd6a2b279397b8fab8c
Macxiiz <tharathorn.wang@gmail.com>
2020-03-16 19:47:16 +0700

    can run fftw3.h YAY!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <complex>
 #include <fftw3.h>
+#include <cmath>
+
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+#define re 0
+#define im 1
+
+int main(){
+    int n = 5;
+    fftw_complex x[n];
+    fftw_complex y[n];
+    for(int i = 0; i < n ; i++){
+        x[i][re] = i + 1;
+        x[i][im] = 0;
+    }
+    fftw_plan plan = fftw_plan_dft_1d(n, x, y, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_execute(plan);
+    fftw_destroy_plan(plan);
+    fftw_cleanup();
+    cout << "FFT =" << endl;
+    for(int i =0;i<n;i++){
+        if(y[i][im] < 0)
+            cout << y[i][re] << " - j" << abs(y[i][im]) << endl;
+        else
+            cout << y[i][re] << " + j" << y[i][im] << endl;
+    }
     return 0;
 }
+
+
